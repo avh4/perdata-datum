deda53417617d5dfff9b4a823d24b48d04e2c822
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-09-03 19:46:45 -0700

    Remove unused interfaces

--- src/main/java/net/avh4/data/datum/transact/IndexStore.java
+++
@@ -1,7 +0,0 @@
-package net.avh4.data.datum.transact;
-
-public interface IndexStore {
-    void add(String indexName, String value, String object);
-
-    void remove(String indexName, String value, String object);
-}
--- src/main/java/net/avh4/data/datum/transact/KeyStore.java
+++
@@ -1,9 +0,0 @@
-package net.avh4.data.datum.transact;
-
-public interface KeyStore {
-    void set(String key, String value);
-
-    String get(String key);
-
-    void remove(String key);
-}
