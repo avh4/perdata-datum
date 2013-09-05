7dab458b7eb23d0c9f5b4b5a628fbe7f1a78696a
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-09-04 22:40:45 -0700

    BTree: Refactor

--- src/main/java/net/avh4/util/data/BTree.java
+++ src/main/java/net/avh4/util/data/BTree.java
@@ -65,17 +65,31 @@ public class BTree {
             if (key.compareTo(keys[i-1]) >= 0) break;
         }
         BTree result = storage.get(nodes[i]).insert(key, value);
+        return integrateInsertResult(i, result);
+    }
+
+    private BTree integrateInsertResult(int i, BTree result) {
+        if (result.vals == null) {
+            return integrateSplitResult(i, result);
+        } else {
+            return replaceChild(i, result);
+        }
+    }
+
+    private BTree integrateSplitResult(int i, BTree result) {
         String[] keys = this.keys.clone();
         long[] nodes = this.nodes.clone();
-        if (result.vals == null) {
-            if (nodes[i+1] != 0) nodes[i+2] = nodes[i+1];
-            nodes[i+1] = result.nodes[1];
-            nodes[i] = result.nodes[0];
-            if (keys[i] != null) keys[i+1] = keys[i];
-            keys[i] = result.keys[0];
-        } else {
-            nodes[i] = storage.write(result);
-        }
+        if (nodes[i+1] != 0) nodes[i+2] = nodes[i+1];
+        nodes[i+1] = result.nodes[1];
+        nodes[i] = result.nodes[0];
+        if (keys[i] != null) keys[i+1] = keys[i];
+        keys[i] = result.keys[0];
+        return new BTree(storage, keys, nodes);
+    }
+
+    private BTree replaceChild(int i, BTree result) {
+        long[] nodes = this.nodes.clone();
+        nodes[i] = storage.write(result);
         return new BTree(storage, keys, nodes);
     }
 
