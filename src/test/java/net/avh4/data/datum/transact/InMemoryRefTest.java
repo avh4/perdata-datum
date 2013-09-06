26245bfb2f1f66804c73590b77ae5484b8afdaee
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-09-06 00:38:00 -0700

    Extract RefContract test

--- src/test/java/net/avh4/data/datum/transact/InMemoryRefTest.java
+++ src/test/java/net/avh4/data/datum/transact/InMemoryRefTest.java
@@ -1,32 +1,8 @@
 package net.avh4.data.datum.transact;
 
-import org.junit.Before;
-import org.junit.Test;
+public class InMemoryRefTest extends RefContract {
 
-import static org.fest.assertions.Assertions.assertThat;
-
-public class InMemoryRefTest {
-    private InMemoryRef<String> subject;
-
-    @Before
-    public void setUp() throws Exception {
-        subject = new InMemoryRef<String>("In a galaxy far, far away");
-    }
-
-    @Test
-    public void hasInitialValue() throws Exception {
-        assertThat(subject.get()).isEqualTo("In a galaxy far, far away");
-    }
-
-    @Test
-    public void commit_updatesValue() throws Exception {
-        subject.commit("It is a period of civil war.");
-        assertThat(subject.get()).isEqualTo("It is a period of civil war.");
-    }
-
-    @Test
-    public void commit_returnsTheValue() throws Exception {
-        final String result = subject.commit("It is a period of civil war.");
-        assertThat(result).isEqualTo("It is a period of civil war.");
+    protected Ref<String> createSubject(String initialValue) {
+        return new InMemoryRef<String>(initialValue);
     }
 }
---
+++ src/test/java/net/avh4/data/datum/transact/RefContract.java
@@ -0,0 +1,34 @@
+package net.avh4.data.datum.transact;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public abstract class RefContract {
+    private Ref<String> subject;
+
+    protected abstract Ref<String> createSubject(String initialValue);
+
+    @Before
+    public void setUp() throws Exception {
+        subject = createSubject("In a galaxy far, far away");
+    }
+
+    @Test
+    public void hasInitialValue() throws Exception {
+        assertThat(subject.get()).isEqualTo("In a galaxy far, far away");
+    }
+
+    @Test
+    public void commit_updatesValue() throws Exception {
+        subject.commit("It is a period of civil war.");
+        assertThat(subject.get()).isEqualTo("It is a period of civil war.");
+    }
+
+    @Test
+    public void commit_returnsTheValue() throws Exception {
+        final String result = subject.commit("It is a period of civil war.");
+        assertThat(result).isEqualTo("It is a period of civil war.");
+    }
+}
